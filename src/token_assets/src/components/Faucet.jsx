0b09a5a583903bb992f39c17d85bc01fa0195303
ibrahimkhan1214 <60528322+ibrahimkhan1214@users.noreply.github.com>
2022-07-13 10:02:10 -0500

    Update Faucet.jsx

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -31,7 +31,7 @@ function Faucet(props) {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 10,000 DANG tokens to {props.userPrincipal}.</label>
+      <label>Get your free DRIG tokens here! Claim 10,000 DRIG tokens to {props.userPrincipal}.</label>
       <p className="trade-buttons">
         <button 
         id="btn-payout" 
